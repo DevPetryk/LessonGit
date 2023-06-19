d63940be1defe071b6bdd902e70bf7d2e199dab8
Oleksandr Petryk <petryk.olexandr@gmail.com>
2023-06-19 21:52:15 +0300

    Create new method AddNumer

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -6,6 +6,16 @@
         {
             Console.WriteLine("*** This is my first console App! ***");
             Console.WriteLine("Hello, World!");
+
+            int var1 = 10;
+            int var2 = 20;
+
+            Console.WriteLine($"Number is: {AddNumber(var1, var2)}");
+        }
+
+        private static int AddNumber(int first, int second)
+        {
+            return first + second;
         }
     }
 }
