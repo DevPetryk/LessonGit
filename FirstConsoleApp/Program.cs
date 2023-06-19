70c516a5a537bd7594d39930a44e6dd96c9137c0
Oleksandr Petryk <petryk.olexandr@gmail.com>
2023-06-19 21:34:50 +0300

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** This is my first console App! ***");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
